5bf73ddc3a3eb921a9443f3d3a4f432075cfb0cd
Garrett Keyes <garrett.keyes@gmail.com>
2025-12-08 13:11:42 -0500

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Garrett Keyes on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
